e6c670060b237258699d93aa6190316d8fe8538f
FireShark224 <126345180+FireShark224@users.noreply.github.com>
2025-04-18 19:03:47 +0200

    encryption_tool.py

--- encryption_tool.py
+++ encryption_tool.py
@@ -9,18 +9,24 @@ key = fernet.Fernet.generate_key() #generate a key
 tool_encrypt = fernet.Fernet(key) #variable that uses the key 
 
 def encryption_service():
+
+    #get the input for the text to encrypt
     text = input("text you want to encrypt:")
     encrypted_text = tool_encrypt.encrypt(text.encode()) #encryption process
     print("your text:", text)
     print("encrypted text:", encrypted_text)
     print("encryption key:", key)
     print("writing encryption data + hashes into a txt file...")
+
+    #compute hashes
     md5sumT = hashlib.md5(text.encode()).hexdigest()
     md5sumK = hashlib.md5(key).hexdigest()
     md5sumE = hashlib.md5(encrypted_text).hexdigest()
     sha256sumT = hashlib.sha256(text.encode()).hexdigest()
     sha256sumK = hashlib.sha256(key).hexdigest()
     sha256sumE = hashlib.sha256(encrypted_text).hexdigest()
+
+    #write data into files
     with open(f'encryption_log_{random.randint(1000, 9999)}.txt', 'w') as file:
         file.write("encrypted text: " + encrypted_text.decode() + "\n") #decode from bytes to readable string
         file.write("key: " + key.decode() + "\n")
