5b66989a421bcbeb5825e9925522de203e67e85a
FireShark224 <126345180+FireShark224@users.noreply.github.com>
2025-04-19 22:03:18 +0200

    hash_comparison_tool.py

--- hash_comparison_tool.py
+++ hash_comparison_tool.py
@@ -1,14 +1,19 @@
 import time
 
+#hash comparison function
 def comparison_service():
+
+    #user input to choose what hashes to compare
     hash1 = input("Input the first hash: ")
     hash2 = input("Input the second hash: ")
+    #print out chosen hashes
     print("First hash: ", hash1)
     print("Second hash: ", hash2)
+    
     if hash1 == hash2:
-        print("Hashes are the same.")
+        print("Hashes are the same.") #if hashes are the same
     else:
-        print("Hashes are not the same.")
+        print("Hashes are not the same.") #if hashes are not the same
     
 comparison_service()
 time.sleep(8)
